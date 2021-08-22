4eca4d0aaaf0ae609627ccae0867d06c002835bd
alexprime100 <alexprime100@gmail.com>
2021-08-22 23:59:24 +0200

    added SQL API

---
+++ Database/DbModels/SqlObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Test.Database.DbModels
+{
+    public class SqlObject
+    {
+        public Dictionary<string, object> Fields { get; set; }
+
+        public SqlObject(Dictionary<string, object> fields)
+        {
+            this.Fields = fields;
+        }
+
+        public SqlObject() { }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (var column in Fields.Keys)
+            {
+                str += Fields[column].ToString() + "; ";
+            }
+            return str;
+        }
+    }
+}
---
+++ Database/DbTools/Join.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Test.Database.DbTools
+{
+    public class Join
+    {
+        public string Table1 { get; set; }
+        public string Table2 { get; set; }
+        public string ForeignKey1 { get; set; }
+        public string ForeignKey2 { get; set; }
+        //public string Sql { get; set; }
+        public Column Column1 { get; set; }
+        public Column Column2 { get; set; }
+
+        public Join(string table1, string table2, string fKey1, string fKey2)
+        {
+            Table1 = table1;
+            Table2 = table2;
+            ForeignKey1 = fKey1;
+            ForeignKey2 = fKey2;
+        }
+
+        public Join(Column column1, Column column2)
+        {
+            Column1 = column1;
+            Column2 = column2;
+        }
+
+        public string GetSql2()
+        {
+            string Sql = " JOIN " + Table2;
+            Sql += " ON " + Table1 + "." + ForeignKey1 + " = " + Table2 + "." + ForeignKey2 + " ";
+
+            return Sql;
+        }
+
+        public string GetSql()
+        {
+            string sql = " JOIN " + Column2.Table;
+            sql += " ON " + Column1.Table + "." + Column1.Name + " = " + Column2.Table + "." + Column2.Name + " ";
+
+            return sql;
+        }
+    }
+}
